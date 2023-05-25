207c3a23aef82a77b14b5820e1a0dad48d8db402
Barney Wilton <B.Wilton1993@gmail.com>
2023-05-25 16:58:32 +0100

    Created first two tests not checking price

---
+++ challenges/candy-search-function/searchCandies.test.js
@@ -0,0 +1,34 @@
+const searchCandies = require('./searchCandies');
+
+describe('searchCandies', () => {
+  it('returns an array of Mars & Maltesers, when passed "Ma" and "10"', () => {
+    subString = 'Ma';
+    maxPrice = 10;
+    expect(searchCandies(subString, maxPrice)).toStrictEqual([ 'Mars', 'Maltesers' ]);
+  })
+
+  it('returns an array of Skitties, Skittles, Starbust when passed "S" and "10"', () => {
+    subString = 'S';
+    maxPrice = 10;
+    expect(searchCandies(subString, maxPrice)).toStrictEqual([ 'Skitties', 'Skittles', 'Starburst' ]);
+  })
+
+  it('returns an array of Mars, when passed "Ma" and "2', () => {
+    subString = 'Ma';
+    maxPrice = 2;
+    expect(searchCandies(subString, maxPrice)).toStrictEqual([ 'Mars' ]);
+  })
+})
+
+
+// > searchCandies('Ma', 10);
+// [ 'Mars', 'Maltesers' ]
+
+// > searchCandies('Ma', 2); // Maltesers excluded: it's more than 2
+// [ 'Mars' ]
+
+// > searchCandies('S', 10); 
+// [ 'Skitties', 'Skittles', 'Starburst' ]
+
+// > searchCandies('S', 4); 
+// [ 'Skitties', 'Skittles' ] // Starbust excluded: it's more than 4
