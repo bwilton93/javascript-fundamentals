f5db7db236b2037032d835a235f10e7e26a2a7d2
Barney Wilton <B.Wilton1993@gmail.com>
2023-05-26 11:26:55 +0100

    Unit tested the basket class using mocked fake candy items

--- challenges/shopping-basket/basket.test.js
+++ challenges/shopping-basket/basket.test.js
@@ -1,3 +1,4 @@
+const exp = require('constants');
 const Basket = require('./basket');
 
 describe('basket', () => {
@@ -17,6 +18,27 @@ describe('basket', () => {
     basket.addItem(fakeCandy);
     expect(basket.items.length).toBe(1);
     expect(basket.getTotalPrice()).toBe(4.99);
+  })
+
+  it('can add multiple items to the basket', () => {
+    const basket = new Basket;
+    let fakeCandy = {
+      getName: () => 'Mars',
+      getPrice: () => 4.99
+    };
+
+    basket.addItem(fakeCandy);
+
+    fakeCandy = {
+      getName: () => 'Skittle',
+      getPrice: () => 3.99
+    };
+
+    basket.addItem(fakeCandy);
+    basket.addItem(fakeCandy);
+
+    expect(basket.items.length).toBe(3);
+    expect(basket.getTotalPrice()).toBe(12.97)
   })
 })
 
