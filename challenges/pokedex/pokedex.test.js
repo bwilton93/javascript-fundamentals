291d3237def7bc1596ace9ac44602de60a1def0a
Barney Wilton <B.Wilton1993@gmail.com>
2023-05-26 16:36:28 +0100

    created a failing test for pokedex catch method

--- challenges/pokedex/pokedex.test.js
+++ challenges/pokedex/pokedex.test.js
@@ -1,8 +1,15 @@
 const Pokedex = require('./pokedex')
+const fetchPokemon = require('./api')
 
 describe(Pokedex, () => {
   it('initialises with an empty pokedex', () => {
     const pokedex = new Pokedex;
     expect(pokedex.myPokemon.length).toBe(0);
+  })
+
+  it('a pokemon can be added to the pokdex using the catch method', () => {
+    const pokedex = new Pokedex;
+    pokedex.catch(fetchPokemon('pikachu'));
+    expect(pokedex.myPokemon.length).toBe(1);
   })
 })
