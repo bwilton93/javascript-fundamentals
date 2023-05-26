0120c01ee3afdeda06f945c394a4b22bee8bef1a
Barney Wilton <B.Wilton1993@gmail.com>
2023-05-26 11:27:13 +0100

    removed misc comments from basket.test.js

--- challenges/shopping-basket/basket.test.js
+++ challenges/shopping-basket/basket.test.js
@@ -41,26 +41,3 @@ describe('basket', () => {
     expect(basket.getTotalPrice()).toBe(12.97)
   })
 })
-
-
-// > const candy = new Candy('Mars', 4.99);
-
-// > candy.getName();
-// 'Mars'
-// > candy.getPrice();
-// 4.99
-
-// > const basket = new ShoppingBasket();
-// > basket.getTotalPrice();
-// 0
-
-// > basket.addItem(candy);
-
-// > basket.getTotalPrice();
-// 4.99
-
-// > basket.addItem(new Candy('Skittle', 3.99));
-// > basket.addItem(new Candy('Skittle', 3.99));
-
-// > basket.getTotalPrice();
-// 12.97
