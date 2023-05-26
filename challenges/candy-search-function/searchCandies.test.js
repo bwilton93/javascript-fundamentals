559e9830d84267973a3299158fe3a0fcec0a8f78
Barney Wilton <B.Wilton1993@gmail.com>
2023-05-26 10:08:59 +0100

    Changed strict equal to toEqual

--- challenges/candy-search-function/searchCandies.test.js
+++ challenges/candy-search-function/searchCandies.test.js
@@ -4,25 +4,25 @@ describe('searchCandies', () => {
   it('returns an array of Mars & Maltesers, when passed "Ma" and "10"', () => {
     subString = 'Ma';
     maxPrice = 10;
-    expect(searchCandies(subString, maxPrice)).toStrictEqual([ 'Mars', 'Maltesers' ]);
+    expect(searchCandies(subString, maxPrice)).toEqual([ 'Mars', 'Maltesers' ]);
   })
 
   it('returns an array of Skitties, Skittles, Starbust when passed "S" and "10"', () => {
     subString = 'S';
     maxPrice = 10;
-    expect(searchCandies(subString, maxPrice)).toStrictEqual([ 'Skitties', 'Skittles', 'Starburst' ]);
+    expect(searchCandies(subString, maxPrice)).toEqual([ 'Skitties', 'Skittles', 'Starburst' ]);
   })
 
   it('returns an array of Mars, when passed "Ma" and "2', () => {
     subString = 'Ma';
     maxPrice = 2;
-    expect(searchCandies(subString, maxPrice)).toStrictEqual([ 'Mars' ]);
+    expect(searchCandies(subString, maxPrice)).toEqual([ 'Mars' ]);
   })
   
   it('returns an array of Skitties, Skittles, when passed "S" and "4"', () => {
     subString = 'S';
     maxPrice = 4;
-    expect(searchCandies(subString, maxPrice)).toStrictEqual([ 'Skitties', 'Skittles' ])
+    expect(searchCandies(subString, maxPrice)).toEqual([ 'Skitties', 'Skittles' ])
   })
 })
 
